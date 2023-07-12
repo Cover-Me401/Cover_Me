3538010fb2ec5f63fc7b32555224c28df6737087
AnthonySinitsa <itonyemail@gmail.com>
2023-07-12 17:13:38 +0000

    fixed filesturuter

--- Docker/modules/indeed_scraper/run.py
+++ Docker/modules/indeed_scraper/run.py
@@ -5,6 +5,9 @@ It scrapes hotel data and saves it to ./results/
 To run this script set the env variable $SCRAPFLY_KEY with your scrapfly API key:
 $ export $SCRAPFLY_KEY="your key from https://scrapfly.io/dashboard"
 """
+
+# poetry run python Docker/modules/indeed_scraper/run.py
+
 import asyncio
 import json
 from pathlib import Path
@@ -15,19 +18,19 @@ output.mkdir(exist_ok=True)
 
 
 async def run():
-    # enable scrapfly cache for basic use
-    indeed.BASE_CONFIG["cache"] = True
+  # enable scrapfly cache for basic use
+  indeed.BASE_CONFIG["cache"] = True
 
-    print("running Indeed scrape and saving results to ./results directory")
+  print("running Indeed scrape and saving results to ./results directory")
 
-    url = "https://www.indeed.com/jobs?q=python&l=Texas"
-    result_search = await indeed.scrape_search(url, max_results=10)
-    output.joinpath("search.json").write_text(json.dumps(result_search, indent=2, ensure_ascii=False))
+  url = "https://www.indeed.com/jobs?q=python&l=Texas"
+  result_search = await indeed.scrape_search(url, max_results=10)
+  output.joinpath("search.json").write_text(json.dumps(result_search, indent=2, ensure_ascii=False))
 
-    jobs = ["4c1e2988b22fa223", "483d39cbe1b6c1fe"]
-    result_jobs = await indeed.scrape_jobs(jobs)
-    output.joinpath("jobs.json").write_text(json.dumps(result_jobs, indent=2, ensure_ascii=False))
+  jobs = ["4c1e2988b22fa223", "483d39cbe1b6c1fe"]
+  result_jobs = await indeed.scrape_jobs(jobs)
+  output.joinpath("jobs.json").write_text(json.dumps(result_jobs, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
-    asyncio.run(run())
+  asyncio.run(run())
