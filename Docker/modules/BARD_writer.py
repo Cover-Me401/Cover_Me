921b4d291845cf8c5cb729ee74f5e7053153150b
Logan Reese <lreese2112@gmail.com>
2023-07-12 00:08:46 +0000

    openai working

--- Docker/modules/BARD_writer.py
+++ Docker/modules/BARD_writer.py
@@ -1,28 +1,13 @@
-# FOURTH
-# Code takes user input from prompts AND the job txt files and runs it through imported AI (bard) to create a cover letter that gets written to a new txt file
-
-from bardapi import Bard
-
+import openai
 import os
-from dotenv import load_dotenv
-from Bard import Chatbot
-from Docker.modules.resume_reader import text
-import sys
-# import program_start
 
-def bard():
-  # This line loads the environment variables from the .env file.
-  load_dotenv()
-  # This line gets the BARD API key from the environment variables.
-  token = os.getenv('_BARD_API_KEY')
-  # This line creates a Chatbot object with the BARD API key.
-  bot = Chatbot(token)
-  # This line asks the BARD API the question "who let dogs out?"
-  question = bot.ask(f'Write a cover letter for this resume:\n{text}')['content']
-  # This line prints the question.
-  print(question)
+openai.api_key = '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'
 
+messages = [{"role": "system", "content": "You are a helpful assistant"}]
 
+while question := input("> "):
+    messages.append({"role": "user", "content": question})
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    print(f'response: {completion["choices"][0]["message"]["content"]}')
 
-if __name__ == "__main__":
-  bard()
+print(completion)
